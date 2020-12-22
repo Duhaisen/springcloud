9a155b277a9eb21c889c634dcfc7a0a007cdc494
duhaisen <15764226651@163.com>
2020-12-22 11:51:43 +0800

    init

---
+++ eureka-server/src/main/java/com/sen/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.sen.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author dhs
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
